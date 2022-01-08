7a4122eea520693acc054195ff17b57f9ef51db6
vv.chekurda <vv.chekurda@tensor.ru>
2022-01-08 14:26:47 +0300

    правка параметров игровой ячейки

--- screens/game/src/main/java/com/chekurda/game_2048/screens/game/presentation/views/cell/params/CellParams.kt
+++ screens/game/src/main/java/com/chekurda/game_2048/screens/game/presentation/views/cell/params/CellParams.kt
@@ -1,17 +1,17 @@
 package com.chekurda.game_2048.screens.game.presentation.views.cell.params
 
 import android.content.Context
+import android.graphics.Color.TRANSPARENT
 import androidx.annotation.ColorInt
-import androidx.annotation.Px
 import androidx.core.content.ContextCompat.getColor
 import com.chekurda.design.custom_view_tools.utils.dp
 import org.apache.commons.lang3.StringUtils.EMPTY
 
 internal data class CellParams(
     val value: String = EMPTY,
-    @Px val textSize: Int = 0,
-    @ColorInt val textColor: Int = 0,
-    @ColorInt val backgroundColor: Int = 0
+    val textSize: Int = 0,
+    @ColorInt val textColor: Int = TRANSPARENT,
+    @ColorInt val backgroundColor: Int = TRANSPARENT
 )
 
 internal fun createCellParams(context: Context, value: Int): CellParams {
