32e9cce01737f335e7bf5270aecf480664efce3c
vv.chekurda <vv.chekurda@tensor.ru>
2022-01-15 14:55:12 +0300

    утилиты для Rect и RectF

---
+++ design/custom_view_tools/src/main/java/com/chekurda/design/custom_view_tools/utils/RectUtils.kt
@@ -0,0 +1,60 @@
+package com.chekurda.design.custom_view_tools.utils
+
+import android.graphics.Rect
+import android.graphics.RectF
+
+/**
+ * Обновить позиции [Rect]
+ */
+fun Rect.update(
+    left: Int = this.left,
+    top: Int = this.top,
+    right: Int = this.right,
+    bottom: Int = this.bottom
+) {
+    set(left, top, right, bottom)
+}
+
+/**
+ * Обновить позиции [RectF]
+ */
+fun RectF.update(
+    left: Float = this.left,
+    top: Float = this.top,
+    right: Float = this.right,
+    bottom: Float = this.bottom
+) {
+    set(left, top, right, bottom)
+}
+
+/**
+ * Преобразование [Rect] в [RectF]
+ */
+fun Rect.toFloat(): RectF =
+    RectF(
+        left.toFloat(),
+        top.toFloat(),
+        right.toFloat(),
+        bottom.toFloat()
+    )
+
+/**
+ * Преобразование [RectF] в [Rect]
+ */
+fun RectF.toInt(): Rect =
+    Rect(
+        left.toInt(),
+        top.toInt(),
+        right.toInt(),
+        bottom.toInt()
+    )
+
+/**
+ * Копирование [Rect] в новый объект
+ */
+fun Rect.copy() = Rect(this)
+
+/**
+ * Копирование [RectF] в новый объект
+ */
+fun RectF.copy() = RectF(this)
