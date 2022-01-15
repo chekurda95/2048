0754774f502d318d30efa273ae3f5a4dad9ec644
vv.chekurda <vv.chekurda@tensor.ru>
2022-01-15 17:04:23 +0300

    добавление механики расположения новой ячейки

--- screens/game/src/main/java/com/chekurda/game_2048/screens/game/presentation/views/field/GameFieldView.kt
+++ screens/game/src/main/java/com/chekurda/game_2048/screens/game/presentation/views/field/GameFieldView.kt
@@ -9,7 +9,10 @@ import com.chekurda.common.surface.SurfaceDrawingThread
 import com.chekurda.common.surface.SurfaceLayout
 import com.chekurda.common.surface.SurfaceLayout.DrawingLayout
 import com.chekurda.game_2048.screens.game.presentation.views.field.config.GameConfig
+import com.chekurda.game_2048.screens.game.presentation.views.field.config.GameConfig.GAME_FIELD_ROW_SIZE
 import com.chekurda.game_2048.screens.game.presentation.views.field.layouts.GameBoard
+import com.chekurda.game_2048.screens.game.presentation.views.field.layouts.cell.GameCell
+import java.lang.Math.random
 import java.lang.RuntimeException
 
 internal class GameFieldView(
@@ -18,6 +21,7 @@ internal class GameFieldView(
 ) : SurfaceView(context), DrawingLayout {
 
     private val board = GameBoard(context)
+    private val cells = HashMap<Int, GameCell>()
 
     init {
         holder.addCallback(SurfaceCallback())
@@ -37,6 +41,36 @@ internal class GameFieldView(
 
     override fun drawLayout(canvas: Canvas) {
         board.draw(canvas)
+        cells.forEach {
+            it.value.draw(canvas)
+        }
+    }
+
+    fun startNewGame() {
+        cells.clear()
+        addNewCell()
+    }
+
+    private fun addNewCell() {
+        val emptyPositions = allPositionList.toMutableList()
+        cells.forEach {
+            emptyPositions.remove(it.key)
+        }
+        if (emptyPositions.isNotEmpty()) {
+            val randomPosition = ((random() * 100).toInt() % emptyPositions.size)
+            val randomValue = ((random() * 100).toInt() % 2 + 1) * 2
+
+            cells[randomPosition] = GameCell(context).apply {
+                value = randomValue
+                setRect(board.getRectForCell(randomPosition))
+            }
+        }
+    }
+
+    private val allPositionList: List<Int> = mutableListOf<Int>().apply {
+        for (i in 0 until GAME_FIELD_ROW_SIZE * GAME_FIELD_ROW_SIZE) {
+            add(i)
+        }
     }
 
     private inner class SurfaceCallback : SurfaceHolder.Callback {
@@ -52,6 +86,7 @@ internal class GameFieldView(
 
         override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
             board.setResolution(width, height)
+            startNewGame()
         }
 
         override fun surfaceDestroyed(holder: SurfaceHolder) {
