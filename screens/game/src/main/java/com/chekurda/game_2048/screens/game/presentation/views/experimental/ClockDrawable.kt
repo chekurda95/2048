416b8f086b94c1a7d090a3ae38ce7e8a73051b83
vv.chekurda <vv.chekurda@tensor.ru>
2022-03-04 13:57:32 +0300

    дополнение документацией, удаление старого компонента

--- screens/game/src/main/java/com/chekurda/game_2048/screens/game/presentation/views/experimental/ClockDrawable.kt
+++ screens/game/src/main/java/com/chekurda/game_2048/screens/game/presentation/views/experimental/ClockDrawable.kt
@@ -3,95 +3,196 @@ package com.chekurda.game_2048.screens.game.presentation.views.experimental
 import android.content.Context
 import android.graphics.*
 import android.graphics.drawable.Drawable
-import androidx.core.graphics.ColorUtils
+import androidx.annotation.ColorInt
+import androidx.annotation.IntRange
+import androidx.annotation.Px
+import androidx.core.graphics.withRotation
 import com.chekurda.design.custom_view_tools.utils.dp
-import kotlin.math.min
 
-internal class ClockDrawable(
-    private val context: Context
-) : Drawable() {
+/**
+ * Drawable часов с анимацией стрелок.
+ *
+ * @author vv.chekurda
+ */
+internal class ClockDrawable(context: Context) : Drawable() {
 
-    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
-    private var paintAlpha = 255
-    private var colorAlpha = 255
-    private val startTime: Long
-    private var color = Color.BLACK
+    private val paint = Paint().apply {
+        isAntiAlias = true
+        style = Paint.Style.STROKE
+        strokeCap = Paint.Cap.ROUND
+    }
+
+    private var hourDurationTimeMs: Int = DEFAULT_HOUR_ROTATION_TIME_MS
+    private var minuteDurationTimeMs: Int = (DEFAULT_HOUR_ROTATION_TIME_MS * DEFAULT_MINUTE_ROTATION_RATIO).toInt()
+    private var startTime = System.currentTimeMillis()
+
+    /**
+     * Установить/получить размер [Drawable] в px.
+     */
+    @get:Px
+    var size: Int
+        get() = minOf(bounds.width(), bounds.height())
+        set(value) {
+            if (value == size) return
+            setBounds(
+                bounds.left,
+                bounds.top,
+                bounds.left + value,
+                bounds.top + value
+            )
+        }
+
+    /**
+     * Установить/получить цвет часов.
+     */
+    @get:ColorInt
+    var color: Int
+        get() = paint.color
+        set(value) {
+            if (value == paint.color) return
+            paint.color = value
+        }
 
     init {
-        paint.style = Paint.Style.STROKE
-        paint.strokeCap = Paint.Cap.ROUND
-        paint.strokeWidth = context.dp(1).toFloat()
-        startTime = System.currentTimeMillis()
+        size = context.dp(DEFAULT_SIZE_DP)
+    }
+
+    /**
+     * Установить периоды вращения стрелок часов.
+     *
+     * @param hourRotationMs период вращения часовой стрелки в мс.
+     * @param minuteRotationMs период вращения минутной стрелки в мс.
+     */
+    fun setRotationDuration(
+        hourRotationMs: Int = DEFAULT_HOUR_ROTATION_TIME_MS,
+        minuteRotationMs: Int = (hourRotationMs * DEFAULT_MINUTE_ROTATION_RATIO).toInt()
+    ) {
+        hourDurationTimeMs = hourRotationMs
+        minuteDurationTimeMs = minuteRotationMs
+    }
+
+    override fun setAlpha(@IntRange(from = 0, to = PAINT_MAX_ALPHA.toLong()) alpha: Int) {
+        if (alpha == paint.alpha) return
+        paint.alpha = alpha
+    }
+
+    override fun setVisible(visible: Boolean, restart: Boolean): Boolean =
+        super.setVisible(visible, restart).also {
+            if (restart) startTime = System.currentTimeMillis()
+        }
+
+    override fun onBoundsChange(bounds: Rect?) {
+        paint.strokeWidth = size * STROKE_WIDTH_RATIO
     }
 
     override fun draw(canvas: Canvas) {
-        val bounds = bounds
-        val r = min(bounds.width(), bounds.height())
-        canvas.drawCircle(
-            bounds.centerX().toFloat(),
-            bounds.centerY().toFloat(),
-            (r shr 1) - context.dp(0.5f).toFloat(),
-            paint
-        )
+        val (centerX, centerY) = bounds.centerX().toFloat() to bounds.centerY().toFloat()
+        val diameter = size
         val currentTime = System.currentTimeMillis()
-        val rotateTime = 1500f
-        val rotateHourTime = rotateTime * 3
-        canvas.save()
-        canvas.rotate(
-            360 * ((currentTime - startTime) % rotateTime) / rotateTime,
-            bounds.centerX().toFloat(),
-            bounds.centerY().toFloat()
-        )
-        canvas.drawLine(
-            bounds.centerX().toFloat(),
-            bounds.centerY().toFloat(),
-            bounds.centerX().toFloat(),
-            bounds.centerY() - context.dp(3).toFloat(),
-            paint
-        )
-        canvas.restore()
-        canvas.save()
-        canvas.rotate(
-            360 * ((currentTime - startTime) % rotateHourTime) / rotateHourTime,
-            bounds.centerX().toFloat(),
-            bounds.centerY().toFloat()
-        )
-        canvas.drawLine(
-            bounds.centerX().toFloat(),
-            bounds.centerY().toFloat(),
-            bounds.centerX() + context.dp(2.3f).toFloat(),
-            bounds.centerY().toFloat(),
-            paint
-        )
-        canvas.restore()
-    }
 
-    fun setColor(color: Int) {
-        if (color != this.color) {
-            colorAlpha = Color.alpha(color)
-            paint.color = ColorUtils.setAlphaComponent(
-                color,
-                (paintAlpha * (colorAlpha / 255f)).toInt()
+        with(canvas) {
+            // Окружность часов
+            drawCircle(centerX, centerY, diameter / 2f, paint)
+            // Минутная стрелка
+            drawArrow(
+                currentTime = currentTime,
+                rotateTime = minuteDurationTimeMs,
+                startX = centerX,
+                startY = centerY,
+                endY = centerY - diameter * MINUTE_ARROW_WIDTH_RATIO
+            )
+            // Часовая стрелка
+            drawArrow(
+                currentTime = currentTime,
+                rotateTime = hourDurationTimeMs,
+                startX = centerX,
+                startY = centerY,
+                endX = centerY + diameter * HOUR_ARROW_WIDTH_RATIO
             )
         }
-        this.color = color
     }
 
     override fun getIntrinsicHeight(): Int =
-        context.dp(12)
+        bounds.width()
 
     override fun getIntrinsicWidth(): Int =
-        context.dp(12)
-
-    override fun setAlpha(i: Int) {
-        if (paintAlpha != i) {
-            paintAlpha = i
-            paint.alpha = (paintAlpha * (colorAlpha / 255f)).toInt()
-        }
-    }
+        bounds.height()
 
     override fun setColorFilter(colorFilter: ColorFilter?) {}
 
     override fun getOpacity(): Int =
         PixelFormat.TRANSPARENT
+
+    /**
+     * Нарисовать стрелку часов.
+     *
+     * @param currentTime текущее время.
+     * @param rotateTime время полного оборота.
+     * @param startX начало стрелки по х.
+     * @param startY начало стрелки по y.
+     * @param endX конец стрелки по x.
+     * @param endY конец стрелки по y.
+     */
+    private fun Canvas.drawArrow(
+        currentTime: Long,
+        rotateTime: Int,
+        startX: Float,
+        startY: Float,
+        endX: Float = startX,
+        endY: Float = startY
+    ) {
+        withRotation(
+            degrees = CIRCLE_DEGREES * ((currentTime - startTime) % rotateTime) / rotateTime.toFloat(),
+            pivotX = startX,
+            pivotY = startY
+        ) {
+            drawLine(
+                startX,
+                startY,
+                endX,
+                endY,
+                paint
+            )
+        }
+    }
 }
+
+/**
+ * Максимальная alpha краски.
+ */
+private const val PAINT_MAX_ALPHA = 255
+
+/**
+ * Стандартный размер часов в dp.
+ */
+private const val DEFAULT_SIZE_DP = 12
+
+/**
+ * Стандартное время оборота часовой стрелки.
+ */
+private const val DEFAULT_HOUR_ROTATION_TIME_MS = 4500
+
+/**
+ * Стандартное соотношение оборотов минутной стрелки к часовой.
+ */
+private const val DEFAULT_MINUTE_ROTATION_RATIO = 1 / 3f
+
+/**
+ * Стандартное соотношение ширины линии краски к размеру часов.
+ */
+private const val STROKE_WIDTH_RATIO = 1 / 10f
+
+/**
+ * Стандартное соотношение длины минутной стрелки к размеру часов.
+ */
+private const val MINUTE_ARROW_WIDTH_RATIO = 1 / 3.75f
+
+/**
+ * Стандартное соотношение длины часовой стрелки к размеру часов.
+ */
+private const val HOUR_ARROW_WIDTH_RATIO = 1 / 5f
+
+/**
+ * Количество градусов в окружности.
+ */
+private const val CIRCLE_DEGREES = 360
